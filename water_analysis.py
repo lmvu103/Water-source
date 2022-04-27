a6a78e19209eaf4a903eab6b98d103573b901759
lmvu103 <lmvu103@gmail.com>
2022-04-27 20:45:44 +0700

    Update water_analysis.py

--- water_analysis.py
+++ water_analysis.py
@@ -4,7 +4,7 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
-from matplotlib.Dates import DateFormatter
+from matplotlib.dates import DateFormatter
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsRegressor
